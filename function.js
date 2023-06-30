c9c61416f975ac08ca43360380967fcfb86de691
Shermin Sultana <rima64549@gmail.com>
2023-07-01 02:30:16 +0600

    function

---
+++ function.js
@@ -0,0 +1,17 @@
+function startFan(){
+    console.log("stand up");
+    console.log("walk towards the switch");
+    console.log("press the switch");
+}
+startFan();
+startFan();
+startFan();
+
+
+
+function bringBottle(water){
+    console.log("i wll drink water");
+    console.log(water);
+}
+bringBottle(100);
+
---
+++ parameter.js
@@ -0,0 +1,30 @@
+function bringsingara(money){
+    console.log("give me one piece");
+    console.log(money);
+}
+bringsingara(199);
+
+
+
+function bringMango(money){
+    console.log("given mango:", money);
+    console.log("take one piece");
+
+}
+var taka = 10;
+bringMango(taka);
+
+// more parameter
+
+function add(num1, num2){
+    console.log("going to add:", num1, num2);
+}
+add(24, 14);
+
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a+b+c+d+e;
+    console.log(sum);
+}
+sum(1, 2, 3, 4, 5);
